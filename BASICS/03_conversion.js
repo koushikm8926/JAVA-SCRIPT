3fb866fdcc10edc2e8f27ceb1a2006b64e2c21c2
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-10 22:20:49 +0530

    type conversion

---
+++ BASICS/03_conversion.js
@@ -0,0 +1,21 @@
+let score = '90'
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let stringToNumber = Number(score)
+
+console.log(typeof(stringToNumber));
+
+let student = true
+console.log(typeof (student));
+let booleanToNumber = Number(student)
+console.log(booleanToNumber);
+
+let number = 1
+console.log(typeof(number));
+let numberToBoolean= Boolean(number)
+console.log(numberToBoolean);
+
+let result;
+console.log(typeof(result));
