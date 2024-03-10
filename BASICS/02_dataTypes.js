63f40b25714d3e44604056c8023aae478c835f38
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-10 12:29:40 +0530

    data types explored

---
+++ BASICS/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict " // treats all the js code as a newer version
+
+
+// alert (3+3) // alert will not work because we are using node not browser 
+
+console.log('koushik') //code readablity should be high
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn=false
+let state; //it is a undefined value
+
+                                     //preamtive datatypes
+//number =>2 to power 53
+//bigint arely used in small application .it is used in some trading application to store huge number of digits
+//string =>""
+//boolean => true/false 
+//null is basically a stand alsone value also null is a representaion of empty value
+//undefined 
+//symbol => to define uniqueness we use symbol 
+
+
+//object
+
+console.log(typeof null) //type of null is object 
+console.log(typeof undefined) // type od undefined is undefined
