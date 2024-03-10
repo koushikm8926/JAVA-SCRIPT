49510830270e547a13af30af725a11e4533cd1d4
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-10 09:12:16 +0530

    learned about variables

---
+++ BASICS/01_variables.js
@@ -0,0 +1,8 @@
+const name= "koushik mondal"; //const cant be chenged after declaring in the code . its constant
+let roll = 24; //let can be chenged
+var city = "kolkata"; //var is shut due to some problem
+let profile;        //undefined
+college="lpu";  // varibles can also be defined without their type declareation but its not an efficent way to declare
+
+console.log(name);
+console.table([name, roll,city,college,profile])
