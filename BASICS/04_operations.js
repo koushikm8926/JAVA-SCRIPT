ed190f81308405a98035d81e8ea9f98cb33234a3
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-11 21:03:54 +0530

    operations

---
+++ BASICS/04_operations.js
@@ -0,0 +1,28 @@
+/********************8operations*******************/
+
+// let val = 3
+// let negValue= -val
+// console.log(negValue);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3/3);
+// console.log(3%3); //remainder
+// console.log(2*2);
+// console.log(2**3); //power 2*2*2
+
+// let str1 = "koushik"
+// let str2 =" Mondal"
+// let str3 = str1+str2
+// console.log(str3);
+
+//we should avoide this type of conversion
+// console.log(2+'2');
+// console.log("2"+2);
+// console.log(2+2+"2");
+// let num1,num2,num3;
+// num1=num2=num3=3+3;
+
+// let number=100
+// number++;
+// console.log(number);
