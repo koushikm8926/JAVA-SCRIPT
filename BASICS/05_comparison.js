a29c5dba4c8d7b70cd3f03d3cb3222126ae1217c
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-12 22:45:30 +0530

    comparesion

---
+++ BASICS/05_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==2);
+// console.log(2!=2);
+
+// console.log("2">1); //js automatically convert string into numbers
+// console.log("02">1);
+
+
+//we should avoide this type of conversions
+// console.log(null >0);
+// console.log(null==0);
+// console.log(null >= 0);
+
+// console.log(undefined==0);
+// console.log(undefined >= 0);
+
+
+// === trple equal to consider to be an strict value checking
+console.log("2"===2);
+console.log(2===2);// triple equal to check value as well as data types 
+console.log("2"==2);//dubble equal to check the value
