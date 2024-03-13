7351d47ed637589be5e8a3683bfc160415f93419
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-13 17:36:13 +0530

    data tyoe summery

---
+++ BASICS/datatype_summery.js
@@ -0,0 +1,20 @@
+// Primitivi DataType
+//7 Types : 1.String, 2.Number, 3.Boolean, 4.Null, 5.undefined , 6.Syymbol, 7.BigInt
+
+
+//non Primitive
+//array , object , functions
+
+// const id =Symbol("123")
+// const id2= Symbol("123")
+// console.log(id==id2);
+
+// const array= ["koushik", "rahul", "mohit", "arijit"] //array
+// const object={
+//     name:'koushik',
+//     roll:12,
+// }//object
+
+const myFunction= = function (){
+    console.log("Hello world");
+}
