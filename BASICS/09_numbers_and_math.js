d7f93d915c3ad4ab417489c1ebd8a5eed10ce3ce
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-17 11:56:57 +0530

    numbers and math

---
+++ BASICS/09_numbers_and_math.js
@@ -0,0 +1,24 @@
+const number = 189.78;
+//const score = new Number(89)
+//console.log(score.toString());
+//console.log(score.toFixed(2));
+//console.log(number.toPrecision(3));
+const bankBalance = 1000000;
+//console.log(bankBalance.toLocaleString());
+
+/*****************MATHS*************************/
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.floor(6.5));
+// console.log(Math.ceil(7.9));
+// console.log(Math.min(5,6,7,2));
+// console.log(Math.max(7,8,4,3));
+// console.log(Math.random());
+
+const min= 10;
+const max =20;
+
+console.log(Math.floor((Math.random()*(max-min +1))+min));
+
