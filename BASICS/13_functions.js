b3a92320455e6fe722abc6524ae49553f00547a1
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 23:12:11 +0530

    addtwo numner function

--- BASICS/13_functions.js
+++ BASICS/13_functions.js
@@ -9,3 +9,9 @@ function myName(){
 }
 
 //myName()
+
+function addTwoNumber(number1,number2){
+    console.log(number1+number2);
+}
+
+addTwoNumber(1,6) // we should always check the number that if it is a number or not 
