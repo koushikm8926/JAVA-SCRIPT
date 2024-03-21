68340e0911624db66f20b5b984360f93fcb30d3f
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 23:12:38 +0530

    add two number functions

--- BASICS/13_functions.js
+++ BASICS/13_functions.js
@@ -14,4 +14,4 @@ function addTwoNumber(number1,number2){
     console.log(number1+number2);
 }
 
-addTwoNumber(1,6) // we should always check the number that if it is a number or not 
+addTwoNumber(1,6) // we should always check the number that if it is a number o
