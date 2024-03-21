091b2f0a826ba7be71479cb6cc3c31f28a695fbe
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 10:07:56 +0530

    objects p1

---
+++ BASICS/12_objects.js
@@ -0,0 +1,8 @@
+const student_data={
+    name:'koushik',
+    school:'Lovely professional University ',
+    degree:'Bachelor Of Computer Application',
+}
+
+//console.table(student_data);
+//console.log(student_data["name"])
