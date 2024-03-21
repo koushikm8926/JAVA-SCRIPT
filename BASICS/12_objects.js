f5c4dd8eb296b4c73a2e2fa785854ebb6a24acd8
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 18:35:19 +0530

    used some methodes

--- BASICS/12_objects.js
+++ BASICS/12_objects.js
@@ -22,4 +22,20 @@ const faceBook = {
     }
 }
 
-console.log(faceBook.userName.userFullName.firstName);
+//console.log(faceBook.userName.userFullName.firstName);
+
+const obj1 = {1:'a',2:'b'}
+const obj2 = {3:'c',4:'d'}
+
+//const obj3 = {obj1, obj2} this is not an efficent way to do 
+const obj3= Object.assign({},obj1, obj2) //this is an efficent way to marge two objects
+                                        //here empty parentasis refers to source and rest all are destination
+const obj4= {...obj1,...obj2}   //sprade operations 
+//console.log(obj4);  //both wiill retuen same output
+//console.log(obj3);  //both wiill retuen same output
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
