30aa7a3f2d9a775a3c044bbe0c50d7b91d7fde5d
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 23:24:19 +0530

    backticked used

--- BASICS/13_functions.js
+++ BASICS/13_functions.js
@@ -17,9 +17,16 @@ function myName(){
 
 // addTwoNumber(1,6) // we should always check the number that if it is a number o
 
-function addTwoNumber(number1,number2){
-    let result = number1+number2
-    return result
+// function addTwoNumber(number1,number2){
+//     let result = number1+number2
+//     return result
+// }
+// const result = addTwoNumber(9,2)
+// console.log(result);
+
+function userLogin(username){
+    return`${username} is logged in successfully`
 }
-const result = addTwoNumber(9,2)
-console.log(result);
+
+const name = userLogin("koushik")
+console.log(name);
