812319758759982a91c12b91fc641dfbb328f830
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-21 22:58:56 +0530

    basic function

---
+++ BASICS/13_functions.js
@@ -0,0 +1,11 @@
+function myName(){
+    console.log("k");
+    console.log("o");
+    console.log("u");
+    console.log("s");
+    console.log("h");
+    console.log("i");
+    console.log("k");
+}
+
+//myName()
