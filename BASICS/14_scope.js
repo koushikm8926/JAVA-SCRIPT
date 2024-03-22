422de17043a3a6d8e37334d96122e3e1c97fa2d7
Koushik Mondal <darkdeavil004@gmail.com>
2024-03-22 20:20:34 +0530

    nested function

--- BASICS/14_scope.js
+++ BASICS/14_scope.js
@@ -1,12 +1,23 @@
 
-let a =2000 //global scope
-if(true){
-    let a =10
-    const b=20
-    var c= 300
-    //local scope
-}
+// let a =2000 //global scope
+// if(true){
+//     let a =10
+//     const b=20
+//     var c= 300
+//     //local scope
+// }
 
-console.log(a)
-//console.log(b)
-console.log(c)
+// console.log(a)
+// //console.log(b)
+// console.log(c)
+
+function one (){
+    const name="Koushik"
+    function two (){
+        console.log(name);
+        const course="javascript"
+    }
+    //console.log(course);
+    two()
+}
+one()
