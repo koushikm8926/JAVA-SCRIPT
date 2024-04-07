25f6bfe7f81bfd51a2751ae2d4f170584ec4a52f
Koushik Mondal <darkdeavil004@gmail.com>
2024-04-07 20:01:46 +0530

    truthy and falsy values discussed

---
+++ CONTROLFLOW/truthy.js
@@ -0,0 +1,22 @@
+const userEmail = []
+
+// if (userEmail) {
+//     console.log("User email got ");
+// }
+// else{
+//     console.log("user dont haveany mail");
+// }
+
+//falsy values = false ,0, -0, bigint 0n, "" , null, undefined , NaN
+//truthy value = "false", "0", " ", [], {}, function(){}
+
+//how to check array 
+//  if (userEmail.length === 0) {
+//     console.log("array is empty");
+//  }
+
+// how to check object
+const rollnumber ={}
+if (Object.keys(rollnumber).length === 0 ) {
+    console.log("object is empty");
+}
