b6512e67b9033eb88fd4b6a29aab88f52422a393
Koushik Mondal <darkdeavil004@gmail.com>
2024-04-07 22:54:06 +0530

    nullish coalescing operator discussed

--- CONTROLFLOW/truthy.js
+++ CONTROLFLOW/truthy.js
@@ -18,5 +18,14 @@ const userEmail = []
 // how to check object
 const rollnumber ={}
 if (Object.keys(rollnumber).length === 0 ) {
-    console.log("object is empty");
+    //console.log("object is empty");
 }
+
+//nullish coalescing operator (??) : null undefined 
+//let val1 = 10;
+let val1 = null ?? 5
+ let val2 = undefined ?? 10
+console.log(val1);
+console.log(val2);
+let val3 = null ?? 20 ?? 30 
+console.log(val3);
