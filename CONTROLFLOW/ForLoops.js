477e69a09b58e6a8341033ea7eb437de1f108107
Koushik Mondal <darkdeavil004@gmail.com>
2024-04-08 09:13:16 +0530

    For loop

---
+++ CONTROLFLOW/ForLoops.js
@@ -0,0 +1,10 @@
+console.log(" Trying to print Table 5 ");
+for (let index = 5; index <= 5; index++) {
+    const element = index;
+    //console.log(element);
+    for (let i = 1; i <= 10; i++) {
+        const element1 =i;
+        console.log(element*element1);
+    }
+    
+}
