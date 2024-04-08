1612ba506fb365465739c3d3eea157d40580fa82
Koushik Mondal <darkdeavil004@gmail.com>
2024-04-08 09:18:33 +0530

    for loop with array

--- CONTROLFLOW/ForLoops.js
+++ CONTROLFLOW/ForLoops.js
@@ -1,10 +1,18 @@
-console.log(" Trying to print Table 5 ");
+//console.log(" Trying to print Table 5 ");
 for (let index = 5; index <= 5; index++) {
     const element = index;
     //console.log(element);
     for (let i = 1; i <= 10; i++) {
         const element1 =i;
-        console.log(element*element1);
+        //console.log(element*element1);
     }
+    
+}
+
+const myArray =["koushikl", "rahul", "mohit"]
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
     
 }
