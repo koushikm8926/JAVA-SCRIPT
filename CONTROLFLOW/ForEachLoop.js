380be7189cce2a62588d8b9ee5e6d667315d0305
Koushik Mondal <darkdeavil004@gmail.com>
2024-04-11 07:59:30 +0530

    foreach with arrow function

--- CONTROLFLOW/ForEachLoop.js
+++ CONTROLFLOW/ForEachLoop.js
@@ -1,5 +1,9 @@
 const conding_language = ["cpp", "java", "rubi", "python"]
 
 conding_language.forEach(function (item){
+   // console.log(item);
+})
+
+conding_language.forEach( (item)=>{
     console.log(item);
 })
